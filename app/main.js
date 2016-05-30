d8e1e0a1a6e53cb87172cf97fccc489398774f1c
Stephen Mckellar <stephen@thingmaker.io>
2016-05-30 13:11:18 +0100

    Jump straight into violino app actually

--- app/main.js
+++ app/main.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Violino extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Violino</h1>
-      </div>
-    );
-  }
-}
+import Violino from './violino.jsx';
 
 ReactDOM.render(<Violino/>, document.getElementById('root'));
---
+++ app/violino.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Violino extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Violino</h1>
+      </div>
+    );
+  }
+}

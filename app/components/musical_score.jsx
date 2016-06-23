f9d67a47afa16f26146170ee7aab85fddbd4962b
Stephen Mckellar <stephen@thingmaker.io>
2016-06-23 13:42:38 +0100

    Show note on score always!

--- app/components/musical_score.jsx
+++ app/components/musical_score.jsx
@@ -32,30 +32,28 @@ const update = (note, showNoteDetails) => {
 
   stave.addClef("treble").setContext(ctx).draw();
 
-  if(showNoteDetails === true) {
-
-    let vNote = new Vex.Flow.StaveNote({ keys: [vexflowFormat(note)], duration: "q" })
-    if(/\#/.test(vexflowFormat(note))) {
-      vNote.addAccidental(0, new Vex.Flow.Accidental("#"));
-    }
-
-    // Create a voice in 4/4
-    let voice = new Vex.Flow.Voice({
-      num_beats: 1,
-      beat_value: 4,
-      resolution: Vex.Flow.RESOLUTION
-    });
-
-    // Add notes to voice
-    voice.addTickables([vNote]);
-
-    // Format and justify the notes to 500 pixels
-    let formatter = new Vex.Flow.Formatter().
-      joinVoices([voice]).format([voice], width);
-
-    // Render voice
-    voice.draw(ctx, stave);
+  // Draw note
+  let vNote = new Vex.Flow.StaveNote({ keys: [vexflowFormat(note)], duration: "q" })
+  if(/\#/.test(vexflowFormat(note))) {
+    vNote.addAccidental(0, new Vex.Flow.Accidental("#"));
   }
+
+  // Create a voice in 4/4
+  let voice = new Vex.Flow.Voice({
+    num_beats: 1,
+    beat_value: 4,
+    resolution: Vex.Flow.RESOLUTION
+  });
+
+  // Add notes to voice
+  voice.addTickables([vNote]);
+
+  // Format and justify the notes to 500 pixels
+  let formatter = new Vex.Flow.Formatter().
+    joinVoices([voice]).format([voice], width);
+
+  // Render voice
+  voice.draw(ctx, stave);
 };
 
 const clear = () => {
